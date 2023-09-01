fb37611325b2a1b0b7bf62d43f0c632fc75c6705
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2023-09-02 00:26:01 +0200

    Update statisticfunctions.h
    added interquartile functions

--- source/library/include/statisticfunctions.h
+++ source/library/include/statisticfunctions.h
@@ -32,23 +32,21 @@ namespace Statisticfunctions
 {
 	//Computes the absolute value of f
 	ROBUSTREGRESSION_API double fabs(double f);
-	ROBUSTREGRESSION_API float  fabs(float f);
 
 	//Computes the factorial n!
 	ROBUSTREGRESSION_API double factorial(size_t n);
 
 	//Computes the standard deviation of an array of size s
-	ROBUSTREGRESSION_API double stdeviation(valarray<double>& errs, size_t s);
+	ROBUSTREGRESSION_API double stdeviation(valarray<double>& errs);
 
 	//Computes the average of an array of size s
-	ROBUSTREGRESSION_API double average(valarray<double>& errs, size_t s);
+	ROBUSTREGRESSION_API double average(valarray<double>& errs);
 
 	//Computes the median of an array of size n
-	ROBUSTREGRESSION_API double median(valarray<double> arr, size_t n);
-	ROBUSTREGRESSION_API double median(valarray<float> arr, size_t n);
+	ROBUSTREGRESSION_API double median(valarray<double> arr );
 
 	//Computes the low median of an array of size n
-	ROBUSTREGRESSION_API double lowmedian(valarray<double> arr, size_t n);
+	ROBUSTREGRESSION_API double lowmedian(valarray<double> arr);
 
 	//Computes the student t distribution for a significance level alpha and an array of size nu from the algorithm in
 	// Smiley W. Cheng, James C. Fu, Statistics & Probability Letters 1 (1983), 223-227
@@ -71,26 +69,31 @@ namespace Statisticfunctions
 	// The estimator was published in
 	// Peter J. Rousseeuw, Christophe Croux, Alternatives to the Median-Absolute Deviation
 	// J. of the Amer. Statistical Assoc. (Theory and Methods), 88 (1993),p. 1273,
-	ROBUSTREGRESSION_API double Q_estimator(valarray<double>& err, size_t s);
+	ROBUSTREGRESSION_API double Q_estimator(valarray<double>& err );
 
 	//Computes the S estimator of rousseuuw for an array of size s. It uses a naive and slow algorithm for copyright reasons
 	//
 	// Peter J. Rousseeuw, Christophe Croux, Alternatives to the Median-Absolute Deviation
 	// J. of the Amer. Statistical Assoc. (Theory and Methods), 88 (1993),p. 1273,
-	ROBUSTREGRESSION_API double S_estimator(valarray<double>& err, size_t s);
+	ROBUSTREGRESSION_API double S_estimator(valarray<double>& err);
 
 	//Computes the mad estimator. It needs the median of the array. The array should have a  size s
-	ROBUSTREGRESSION_API double MAD_estimator(valarray<double>& err,double &m ,size_t s);
+	ROBUSTREGRESSION_API double MAD_estimator(valarray<double>& err, double& m);
 
 	//Computes the t estimator of an array of size s.
 	// The estimator was described in
 	// Peter J. Rousseeuw, Christophe Croux, Alternatives to the Median-Absolute Deviation
 	// J. of the Amer. Statistical Assoc. (Theory and Methods), 88 (1993),p. 1273,
-	ROBUSTREGRESSION_API double T_estimator(valarray<double>& err, size_t s);
+	ROBUSTREGRESSION_API double T_estimator(valarray<double>& err);
 
 	//Computes the biweight midvariance for one step for an array of size s,
 	//  It expects the median m and the size of the array 
 	//   T. C. Beers,K. Flynn and K. Gebhardt,  Astron. J. 100 (1),32 (1990)
-	ROBUSTREGRESSION_API double onestepbiweightmidvariance(valarray<double>& err, double&m,size_t s);
-	
+	ROBUSTREGRESSION_API double onestepbiweightmidvariance(valarray<double>& err, double& m);
+
+	//Compute the interquartiles
+	ROBUSTREGRESSION_API double Q3(valarray<double> m);
+	ROBUSTREGRESSION_API double Q1(valarray<double> m);
+
+
 }
