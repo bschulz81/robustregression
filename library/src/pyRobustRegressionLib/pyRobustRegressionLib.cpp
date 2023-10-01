d69990e8fa6af088b35c78d652882cbff9ea8d9f
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2023-10-01 09:01:39 +0200

    Update pyRobustRegressionLib.cpp
    update of the documentation

--- library/src/pyRobustRegressionLib/pyRobustRegressionLib.cpp
+++ library/src/pyRobustRegressionLib/pyRobustRegressionLib.cpp
@@ -131,7 +131,9 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 		"Compute the Quartile Q3");
 
 	Statisticfunctions.doc() = "Contains a set of statistic functtions\n factorial:Computes the factorial n!\n\n stdeviation : Computes the standard deviation of an "
-		"array\n\n average : Computes the average of an array\n\n lowmedian : lowmedian\n\n t : "
+		"array\n\n average : Computes the average of an array\n\n lowmedian : lowmedian: Computes the low median of an array (equal to the "
+		"median if the array has odd members, for even members, the low median returns the element that would appear by one element below "
+		"the middle if the array were sorted. \n\n t :  "
 		"Computes the Student t distribution for a significance level alpha and an array of size nu from the algorithm "
 		"in Smiley W.Cheng, James C.Fu, Statistics& Probability Letters 1 (1983), 223 - 227\n\n crit : Computes the critical values "
 		"of the student t distribution for significance level alpha and an array of size N\n\n peirce : Computes the peirce criterium "
@@ -179,12 +181,18 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 		;
 
 	linearregression.def("median_linear_regression", &Linear_Regression::median_linear_regression,
-		"computes a linear regression. datapoints are two valarrays x and y.the result is put into res.");
+						"computes Siegel's repeated median linear regression, which is more robust against outliers. \n"
+						"datapoints are given by two valarrays x and y. the result is put into the third parameter res. For the description of the algorithm, see \n"
+						"Andrew F.Siegel, Robust regression using repeated medians.Bionaetrika, 69(1) : 242–244, 1982, \n"
+						"Andrew Stein and Michael Werman. 1992. Finding the repeated median regression line, in Proceedings of the third annual "
+						"ACM - SIAM symposium on Discrete algorithms(SODA '92). Society for Industrial and Applied Mathematics, USA, 409–413, "
+						" https://dl.acm.org/doi/10.5555/139404.139485 \n Note that a comparably slow algorithm is used in this current implementation "
+						"of the library. Parameters are similar as in the linear regression. \n It is slower than linear "
+						"regression and for large and many outliers, it also does not yield precise results. ");
 
 	linearregression.def("linear_regression", &Linear_Regression::linear_regression,
-		"Computes a median linear regression, which is more robust against outliers. "
-		"Parameters are similar as in the linear regression. \nIt is slower than linear "
-		"regression and for large and many outliers, it also does not yield precise results. ");
+						"Computes a simple linear regression. Datapoints are given by two valarrays x and y. The result is put into the third parameter res.");
+		
 
 	py::module matrixcode = m.def_submodule("MatrixCode");
 
@@ -338,14 +346,30 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 								;
 
 							py::enum_< LossFunctions::errorfunction_name>(lossfunctions, "errorfunction_name")
+
 								.value("huberlossfunction", LossFunctions::errorfunction_name::huberlossfunction,
-									"The loss function is given by Huber's loss function")
+									"The loss function is given by Huber's loss function per pointnumber")
 
 								.value("squaredresidual", LossFunctions::errorfunction_name::squaredresidual,
-									"The loss function is given by the squared residuals")
+									"The loss function is given by the err^2/pointnumber, where err is the residual")
 
 								.value("absolutevalue", LossFunctions::errorfunction_name::absolutevalue,
-									"The Loss function is given by the absolute values of the residuals")
+									"The Loss function is given by |err|/pointnumber, where err is the residual")
+
+								.value("logcosh", LossFunctions::errorfunction_name::logcosh,
+									"The Loss function is given by ln(cosh(err)/pointnumber, where err is the residual")
+
+								.value("quantile", LossFunctions::errorfunction_name::quantile,
+									"The Loss function is given by (gamma-1)*err/pointnumber if err<0 and gamma*err/pointnumber if err>0, where err is the residual\n"
+									"Note that quantile is an asymmetric loss function.Therefore, it should mostly be used with the linear robust curve fitting "
+									"algorithms, since then it is only used for outlier removal. If the quantile loss function is used with the non - linear robust "
+									"algorithms it is likely to confuse the Levenberg-Marquardt algorithm.")
+
+
+								.value("custom", LossFunctions::errorfunction_name::custom,
+									"The Loss per point is given by loss_pp(Y[i],Ypred[i]), where Y[i] is the Y value of the datapoint (Y[{i],X[i]) and Y_pred[i]"
+									"is given by Ypred[i]=f(X[i]), when f(X) is the curve fit. Zhe entire error by agg_err in the errorfunction. if agg_err is not set,"
+									"the entire error is given by the sum of the calls of loss_pp for every point i")
 								.export_values()
 								;
 
@@ -353,15 +377,73 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 								.def(py::init<>())
 
 								.def_readwrite("lossfunction", &LossFunctions::errorfunction::lossfunction,
-									"The metric with which the loss function is calculated")
+									"The metric with which the loss function is calculated. \n Note that if the lossfunction is given an argument of linear robust "
+									"regression algorithms, then the lossfunction is only used for the outlier detection, since the linear curve fitting algorithms "
+									"that form the basis of the robust methods are either simple linear regression or Siegel's repeated median algorithm, that have "
+									"their own error metric which they minimize.\n"
+									" In contrast, when a non-linear robust algorithm is used, the loss function is also used for the Levenberg-Marquardt algorithm.")
 
 								.def_readwrite("huberslossfunction_border", &LossFunctions::errorfunction::huberslossfunction_border,
-									"If Huber's loss function is used, this determines its borders delta")
+									"If Huber's loss function is used, this determines the parameter delta in H_delta(err)=0.5*err^2 "
+									"if err<=delta or H_delta=delta*(|err|-0.5*delta for err>delta")
 
-								.doc() = "Describes the metric how errors are computed. the field lossfunction describes the name of the metric for the errors. It can be  huberlossfunction, "
-								"squaredresidual or absolutevalue. If Huber's loss function is chosen, the field huberslossfunction_border describes the delta parameter "
-								"after which loss(error) function will behave as | error | instead of error ^ 2."
-								;
+								.def_readwrite("gamma", &LossFunctions::errorfunction::gamma,
+									"If quantile is used as an error function, this determines the gamma value in the loss function "
+									"L_gamma(err)=(gamma-1)*err/pointnumber if err<0 and gamma*err/pointnumber if err>0")
+
+								.def_readwrite("loss_perpoint", &LossFunctions::errorfunction::loss_perpoint,
+									"The loss function per point. If custom is used, then is a function pointer to a custom loss function. "
+									"It is called with a double value y that represents the Y value of the i-th data point, a double  valuef(X[i]), "
+									"where f is the fitted function and a non negative integer which is given by the number of points to be fitted. "
+									"The last parameter can be used to scale the loss function by the pointnumber."
+									"The function should return the value of a loss function for the residual between y and f. It is only used if custom is set as a "
+									"loss function. If a builtin loss function is used, loss_perpoint should not be initialised. if custom is used, and aggregate_error is "
+									"not initialised, then aggregate_error is simply set to the sum of the results of all calls of loss_perpoint. "
+									" If custom is not used loss_perpoint does not need to be set")
+
+								.def_readwrite("aggregate_err", &LossFunctions::errorfunction::aggregate_err,
+									"The aggregate error function. if custom is used as a loss function, then aggregate_error is a function pointer that should call function "
+									"that computes an aggregated error from an array of errors per point given by loss_pp. "
+									"if it is not initialised, then aggregate_error is simply computed as the sum of the results of calls to loss_pp. \n"
+									"When one defines custom error functions, it is usually best to compute some average error instead of a sum. "
+									"The reason is that the robust curve fitting methods remove points based on the error of the fit, and if the fit is not exactly "
+									"perfect, using the sum would mean that removing points would always yield a smaller error. This would usually result in a curve fit "
+									"that would use not enough points. As a result, dividing the the aggregate error the loss function per point by the point number "
+									"is often a good idea. if custom is not used loss_perpoint does not need to be set ")
+
+								.doc() = "Describes the metric how errors are computed. The field lossfunction describes the name of the metric for the errors. It can be"  
+								"huberlossfunction, squaredresidual or absolutevalue, logcosh, quantile or custom. \n"
+								"Note that if the lossfunction is given an argument of linear robust regression algorithms, then the lossfunction is only used "
+								"for the outlier detection, since the linear curve fitting algorithms that form the basis of the robust methods are either simple "
+								"linear regression or Siegel's repeated median algorithm, that have their own error metric which they minimize.\n"
+								"In contrast, when a non-linear robust algorithm is used, the loss function is also used for the Levenberg-Marquardt algorithm. "
+								"This discourages to use the quantile loss function together with a non-linear robust algorithm. The quantile loss function is "
+								"asymmetric but the Levenberg-Marquardt algorithm is numerically sensitive and seems to need a symmetric loss function. \n\n"
+								"If Huber's loss function is chosen, the field huberslossfunction_border=delta describes the delta parameter "
+								"in Huber_delta(err)=delta*(|err|-1/2*delta)/pointnumber for err>delta and Huber_delta(err)=1/2*err^2/pointnumber for err<=delta."
+								"By default, huberslossfunction_border is set to 10, but this choice is arbitrary should be changed to case of the user\n\n"
+								"If squaredresidual is chosen, the loss function is given by err^2/pointnumber, where err is the residual.\n\n"
+								"If absolutevalue is chosen, the loss function is given by |err|/pointnumber, where err is the residual.\n\n"
+								"If logcosh is chosen, the loss function is given by log(cosh(err)/pointnumber, where err is the residual.\n\n"
+								"If quantile is chosen, the loss function is given by (gamma-1)*err/pointnumber if err<0 and to gamma*err/pointnumber, "
+								"where err is the residual. By default gamma is set to 0.25\n"
+								"Note that quantile is an asymmetric loss function.Therefore, it should mostly be used with the linear robust curve fitting "
+								"algorithms, since then it is only used for outlier removal. If the quantile loss function is used with the non - linear robust "
+								"algorithms it is likely to confuse the Levenberg-Marquardt algorithm. \n\n"
+								"if custom is chosen, then the pointer to a function loss_pp(Y,Y_pred) needs to be initialized with a function that returns a double "
+								"value and is called with a double value  Y[i] of a datapoint (Y[i], X[i]) and another double value f(X[i], where f is the function "
+								"of the curve fit for that datapoint. This function should compute the loss function for an individual datapoint (Y[i], X[i]) "
+								" and the curve fit f(X[i]) at that point.\n"
+								"With custom set, one may also initialize the function pointer aggregate_error with a function aggregate_error(errorarray). "
+								"It is called with an array of values generated by calls to loss_pp. aggregate_error should return a double value representing " 
+								"the accumulated loss between the data and the curve fit for all datapoints.\n"
+								"Usually, it is good to chose some sort of average error for this function.The robust curve fitting "
+								"methods remove points based on this value. If one would set this function to a sum and the curve fit of neither point were without "
+								"error, then, one would decrease the error by removing all points. This would usually remove so many points that one would not get "
+								"accurare results anymore. So an average error, where one divides the accumulated error by the pointnumber (i.e. the size of "
+								"errorarray) is usually a better measure\n"
+								"If custom is used and aggregate_error is not initialised, then the entire error is set to the sum of the results of the calls of "
+								"loss_pp divided by the pointnumber.\n";
 
 
 
@@ -434,8 +516,8 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 								.def_readwrite("tolerable_error", &Non_Linear_Regression::control::tolerable_error,
 									"Sets a tolerable error after which the iteration of the Levenberg-Marquardt algorithm stops")
 
-								.doc() = "Describes the parameters of the Levenberg-Marquardt algorithm with fields lambda, increlemt, decrement,h which are similar as in "
-								"https://en.wikipedia.org/wiki/Levenberg%E2%80%93Marquardt_algorithm additionally, it has the fields precision, which stop the algorithm after "
+								.doc() = "Describes the parameters of the Levenberg-Marquardt algorithm with fields lambda, increment, decrement,h which are similar as in "
+								"https://en.wikipedia.org/wiki/Levenberg%E2%80%93Marquardt_algorithm Additionally, it has the fields precision, which stop the algorithm after "
 								"its improval is only increased in an iteration by a margin smaller than precision,  stop_nonlinear_curve_fitting_after_iterations "
 								"which stop the algorithm after a given number of iterations, stop_nonlinear_curve_fitting_after_seconds, which stops the algorithm "
 								"after a given time and	tolerable_error, which stops the algorithm once the error is deemed small enough by the given margin"
@@ -575,7 +657,7 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 								"stop_after_numberofiterations_without_improvement : Stops the robust regression after a given number of iterations with no improvement  \n"
 								"maximum_number_of_outliers : Sets a maximum number of outliers that the algorithm can find. Per default or if set to zero, it will designate at maximum 30 % "
 								"of the points as outliers";
-							;
+						
 
 							py::class_< Robust_Regression::linear_algorithm_control, Robust_Regression::control, LossFunctions::errorfunction>(robustregression, "linear_algorithm_control")
 								.def(py::init<>())
@@ -588,8 +670,7 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 
 								.doc() = "Controls the behavior of the robust linear regression algorithms\n inherits from  Robust_Regression::control and  LossFunctions::errorfunction. \n"
 								"use_median_regression determines whether median regression should be used\n tolerable_error gives a margin for the residual where the algorithm stops if the"
-								"residual becomes smaller"
-								;
+								"residual becomes smaller";
 
 							py::class_< Robust_Regression::nonlinear_algorithm_control, Robust_Regression::control, Non_Linear_Regression::control>(robustregression,
 								"nonlinear_algorithm_control")
@@ -656,4 +737,3 @@ PYBIND11_MODULE(pyRobustRegressionLib, m) {
 							robustregression.def("linear_loss_function", &Robust_Regression::linear_loss_function,
 								"Implements a loss function for a linear curve fit that computes the residuals "
 								"for every point given some metric (squared residuals, Huber's loss function, absolute value), and the sum of these residuals");
-};
