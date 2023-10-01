5768d3a467802ebd33472c4ee4ab4b67cd7ff8c9
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2023-10-01 09:03:17 +0200

    Update matrixcode.cpp
    more open-mp simd statements, 
    compatibility with gcc

--- library/src/matrixcode.cpp
+++ library/src/matrixcode.cpp
@@ -31,38 +31,31 @@ using signed_size_t = std::make_signed_t<std::size_t>;
 using namespace std;
 
 
-#ifdef GNUCOMPILER
-#define _inline inline
-#endif
-
-#ifdef CLANGCOMPILER
-#define _inline inline
-#endif
 
 
-
-ROBUSTREGRESSION_API inline  Vector::Vector(const size_t s) {
+ROBUSTREGRESSION_API   Vector::Vector(const size_t s) {
 	m.resize(s, 0.0);
 }
 
-ROBUSTREGRESSION_API inline  Vector::Vector(valarray<double> *m1)
+ROBUSTREGRESSION_API   Vector::Vector(valarray<double> *m1)
 {
 	m = *m1;
 }
-ROBUSTREGRESSION_API  inline Vector::Vector(const valarray<double> m1)
+ROBUSTREGRESSION_API   Vector::Vector(const valarray<double> m1)
 {
 	m = m1;
 }
 
-ROBUSTREGRESSION_API inline void Vector::Resize(const size_t t)
+ROBUSTREGRESSION_API  void Vector::Resize(const size_t t)
 {
 	m.resize(t);
 }
 
-ROBUSTREGRESSION_API inline Vector Vector::operator=(Vector& B)
+ROBUSTREGRESSION_API  Vector Vector::operator=(Vector& B)
 {
 	size_t s = B.Size();
 	m.resize(s);
+
 	for (size_t j = 0; j < s;j++)
 	{
 		m[j] = B(j);
@@ -71,35 +64,34 @@ ROBUSTREGRESSION_API inline Vector Vector::operator=(Vector& B)
 }
 
 
-ROBUSTREGRESSION_API inline Vector Vector::operator=(Vector* B)
+ROBUSTREGRESSION_API  Vector Vector::operator=(Vector* B)
 {
 	m = *B;
 	return *this;
 }
 
-ROBUSTREGRESSION_API inline Vector Vector::operator+(const Vector& B) const{
+ROBUSTREGRESSION_API  Vector Vector::operator+(const Vector& B) const{
 	Vector sum(m.size());
 
-
+#pragma omp simd
 	for (size_t i = 0; i < m.size(); i++)
 	{
 		sum(i) = m[i] + B(i);
 	}
 	return sum;
 }
-ROBUSTREGRESSION_API inline Vector Vector::operator -(const Vector& B)const {
+ROBUSTREGRESSION_API  Vector Vector::operator -(const Vector& B)const {
 	Vector sum(m.size());
 
-#pragma omp parallel for
+#pragma omp simd
 	for (signed_size_t i = 0; i < m.size(); i++)
 	{
 		sum(i) = m[i] - B(i);
 	}
 	return sum;
 }
-ROBUSTREGRESSION_API inline double Vector::operator*(const Vector& B)const {
+ROBUSTREGRESSION_API  double Vector::operator*(const Vector& B)const {
 	double sum = 0;
-
 #pragma omp simd
 	for (size_t i = 0; i < m.size(); i++)
 	{
@@ -109,7 +101,7 @@ ROBUSTREGRESSION_API inline double Vector::operator*(const Vector& B)const {
 }
 
 
-ROBUSTREGRESSION_API inline Vector Vector::operator*(const double& B)const {
+ROBUSTREGRESSION_API  Vector Vector::operator*(const double& B)const {
 	Vector sum(m.size());
 
 	for (size_t i = 0; i < m.size(); i++)
@@ -118,7 +110,7 @@ ROBUSTREGRESSION_API inline Vector Vector::operator*(const double& B)const {
 	}
 	return sum;
 }
-ROBUSTREGRESSION_API inline Vector Vector::operator/(const double& B) const{
+ROBUSTREGRESSION_API  Vector Vector::operator/(const double& B) const{
 	Vector sum(this->Size());
 	if (B != 0)
 	{
@@ -130,12 +122,13 @@ ROBUSTREGRESSION_API inline Vector Vector::operator/(const double& B) const{
 	return sum;
 }
 
-
-ROBUSTREGRESSION_API inline double& Vector::operator()(const size_t i)
+ROBUSTREGRESSION_API  double& Vector::operator()(const size_t i)
 {
 	return  m[i];
 }
-ROBUSTREGRESSION_API inline const double& Vector::operator()(const size_t i)const
+
+
+ROBUSTREGRESSION_API  const double& Vector::operator()(const size_t i)const
 {
 	return m[i];
 }
@@ -147,7 +140,7 @@ ROBUSTREGRESSION_API inline const double& Vector::operator()(const size_t i)cons
 }
 
 
- ROBUSTREGRESSION_API inline void Matrixcode::printvector(const Vector &v)
+ ROBUSTREGRESSION_API  void Matrixcode::printvector(const Vector &v)
  {
 	 for (size_t i = 0; i < v.Size(); i++)
 	 {
@@ -157,31 +150,30 @@ ROBUSTREGRESSION_API inline const double& Vector::operator()(const size_t i)cons
  }
 
 
- ROBUSTREGRESSION_API inline Matrix::Matrix (const size_t rows, const size_t columns, valarray<double> m1)
+ ROBUSTREGRESSION_API  Matrix::Matrix (const size_t rows, const size_t columns, valarray<double> m1)
   {
 	  r = rows;
 	  c = columns;
 	  m = m1;
   }
 
- ROBUSTREGRESSION_API inline Matrix::Matrix(const size_t rows, const size_t columns, valarray<double>* m1)
+ ROBUSTREGRESSION_API  Matrix::Matrix(const size_t rows, const size_t columns, valarray<double>* m1)
   {
 	  r = rows;
 	  c = columns;
 	  m = *m1;
   }
 
- ROBUSTREGRESSION_API inline Matrix::Matrix(const size_t rows, const size_t columns) {
+ ROBUSTREGRESSION_API  Matrix::Matrix(const size_t rows, const size_t columns) {
 	 r = rows;
 	 c = columns;
 	 m.resize(r * c, 0.0);
  }
 
 
-ROBUSTREGRESSION_API inline  Matrix Matrixcode::Identity(const size_t rows, const size_t columns)
+ROBUSTREGRESSION_API   Matrix Matrixcode::Identity(const size_t rows, const size_t columns)
 {
 	Matrix m(rows, columns);
-
 	for (size_t i = 0; i < rows; i++)
 	{
 		if (i < columns)
@@ -191,24 +183,22 @@ ROBUSTREGRESSION_API inline  Matrix Matrixcode::Identity(const size_t rows, cons
 	}
 	return m;
 }
-ROBUSTREGRESSION_API inline Matrix Matrixcode::Diagonal(const Matrix &m) 
+ROBUSTREGRESSION_API  Matrix Matrixcode::Diagonal(const Matrix &m) 
 {
 	size_t u = m.Rows();
 	Matrix m1(u, m.Columns());
-
 	for (size_t i = 0; i < u; i++)
 	{
 		m1(i, i) = m(i, i);
 	}
 	return m1;
 }
-ROBUSTREGRESSION_API inline Vector Matrixcode::Gaussian_algorithm(const Matrix &m,const  Vector& v)
+ROBUSTREGRESSION_API  Vector Matrixcode::Gaussian_algorithm(const Matrix &m,const  Vector& v)
 {
-	  Vector result(&(Matrixcode::Gaussian_algorithm(m,(valarray<double>)v)));
-	  return result;
+	  return (Vector)Matrixcode::Gaussian_algorithm(m,(valarray<double>)v);
 }
 
-ROBUSTREGRESSION_API inline void Matrix::SwapRows(const size_t row1, const size_t row2) 
+ROBUSTREGRESSION_API  void Matrix::SwapRows(const size_t row1, const size_t row2) 
 {
 
 	  if (row1 != row2) 
@@ -225,12 +215,11 @@ ROBUSTREGRESSION_API inline void Matrix::SwapRows(const size_t row1, const size_
 
 
 
-ROBUSTREGRESSION_API inline valarray<double> Matrixcode::Gaussian_algorithm(const Matrix &m, const  valarray<double>& v)
+ROBUSTREGRESSION_API  valarray<double> Matrixcode::Gaussian_algorithm(const Matrix &m, const  valarray<double>& v)
   {
 		size_t u = m.Rows();
 		size_t t = m.Columns();
 
-		//Create a copy of the matrix
 		Matrix m2(u, t + 1);
 
 		for (size_t i = 0; i < u; i++)
@@ -247,35 +236,36 @@ ROBUSTREGRESSION_API inline valarray<double> Matrixcode::Gaussian_algorithm(cons
 		for (size_t j = 0; j < t; j++)
 		{
 			size_t pivot_row = j;
-
-			// Find the pivot row with the largest element in the column
-			for (size_t i = j + 1; i < u; i++) {
+			for (size_t i = j + 1; i < u; i++)
+			{
 				if (std::abs(m2(i, j)) > std::abs(m2(pivot_row, j))) {
 					pivot_row = i;
 				}
 			}
 
-			// Swap rows to bring the pivot element to the diagonal
 			if (pivot_row != j)
 				m2.SwapRows(j, pivot_row);
 
-			// Gaussian elimination steps using pivoting
 			double pivot_value = m2(j, j);
-
-			for (size_t k = j; k < t + 1; k++) {
+			for (size_t k = j; k < t + 1; k++)
+			{
 				m2(j, k) /= pivot_value;
 			}
 
-			for (size_t i = j + 1; i < u; i++) {
+			for (size_t i = j + 1; i < u; i++)
+			{
 				double factor = m2(i, j);
-				for (size_t k = j; k < t + 1; k++) {
+
+				for (size_t k = j; k < t + 1; k++) 
+				{
 					m2(i, k) -= factor * m2(j, k);
 				}
 			}
 		}
 
 
-		for (size_t i = t; i > 0; i--) {
+		for (size_t i = t; i > 0; i--) 
+		{
 			double sum = 0.0;
 			for (size_t j = (size_t)i; j < t; j++) {
 				sum += m2(i-1, j) * result[j];
@@ -289,16 +279,15 @@ ROBUSTREGRESSION_API inline valarray<double> Matrixcode::Gaussian_algorithm(cons
 
 
 
-ROBUSTREGRESSION_API inline void Matrix::Resize(const size_t rows,const size_t columns)
+ROBUSTREGRESSION_API  void Matrix::Resize(const size_t rows,const size_t columns)
 {
 	r = rows;
 	c = columns;
 	m.resize(r * c, 0.0);
 }
 
-ROBUSTREGRESSION_API inline Matrix Matrix::operator+(const Matrix& B) const{
+ROBUSTREGRESSION_API  Matrix Matrix::operator+(const Matrix& B) const{
 	Matrix sum(c, r);
-#pragma omp parallel for
 	for (signed_size_t i = 0; i < r; i++)
 	{
 		for (size_t j = 0; j < c; j++)
@@ -309,7 +298,7 @@ ROBUSTREGRESSION_API inline Matrix Matrix::operator+(const Matrix& B) const{
 	return sum;
 }
 
-ROBUSTREGRESSION_API inline  Matrix Matrix::operator-(const Matrix& B) const{
+ROBUSTREGRESSION_API   Matrix Matrix::operator-(const Matrix& B) const{
 	Matrix diff(r, c);
 	for (signed_size_t i = 0; i < r; i++)
 	{
@@ -322,7 +311,7 @@ ROBUSTREGRESSION_API inline  Matrix Matrix::operator-(const Matrix& B) const{
 	return diff;
 }
 
-ROBUSTREGRESSION_API inline Matrix Matrix::operator*(const Matrix& B) const{
+ROBUSTREGRESSION_API  Matrix Matrix::operator*(const Matrix& B) const{
 	Matrix multip(r, B.Columns());
 
 	if (c == B.Rows())
@@ -332,6 +321,7 @@ ROBUSTREGRESSION_API inline Matrix Matrix::operator*(const Matrix& B) const{
 			for (size_t j = 0; j < B.Columns(); j++)
 			{
 				double temp = 0.0;
+#pragma omp simd
 				for (size_t k = 0; k < c; k++)
 				{
 					temp += (*this)(i, k) * B(k, j);
@@ -343,7 +333,7 @@ ROBUSTREGRESSION_API inline Matrix Matrix::operator*(const Matrix& B) const{
 	return multip;
 }
 
-ROBUSTREGRESSION_API inline Matrix Matrix::operator*(const double& scalar) const{
+ROBUSTREGRESSION_API  Matrix Matrix::operator*(const double& scalar) const{
 	Matrix result(r, c);
 	for (signed_size_t i = 0; i < r; i++)
 	{
@@ -355,7 +345,7 @@ ROBUSTREGRESSION_API inline Matrix Matrix::operator*(const double& scalar) const
 	return result;
 }
 
-ROBUSTREGRESSION_API inline Vector Matrix::operator*(const Vector& B)const {
+ROBUSTREGRESSION_API  Vector Matrix::operator*(const Vector& B)const {
 
 	Vector result((this)->Rows());
 	if (B.Size() == (this)->Columns())
@@ -363,6 +353,7 @@ ROBUSTREGRESSION_API inline Vector Matrix::operator*(const Vector& B)const {
 		for (signed_size_t i = 0; i < r; i++)
 		{
 			double sum = 0;
+#pragma omp simd
 			for (size_t j = 0; j < c; j++)
 			{
 				sum += (*this)(i, j) * B(j);
@@ -373,7 +364,7 @@ ROBUSTREGRESSION_API inline Vector Matrix::operator*(const Vector& B)const {
 	return result;
 }
 
-ROBUSTREGRESSION_API inline valarray<double> Matrix::operator*(const valarray<double>& B) const{
+ROBUSTREGRESSION_API  valarray<double> Matrix::operator*(const valarray<double>& B) const{
 
 	valarray<double> result((this)->Rows());
 
@@ -382,6 +373,7 @@ ROBUSTREGRESSION_API inline valarray<double> Matrix::operator*(const valarray<do
 		for (signed_size_t i = 0; i < r; i++)
 		{
 			double sum = 0;
+#pragma omp simd
 			for (size_t j = 0; j < c; j++)
 			{
 				sum += (*this)(i, j) * B[j];
@@ -394,8 +386,9 @@ ROBUSTREGRESSION_API inline valarray<double> Matrix::operator*(const valarray<do
 
 
 
-ROBUSTREGRESSION_API inline Matrix Matrix::operator/(const double&scalar) const {
+ROBUSTREGRESSION_API  Matrix Matrix::operator/(const double&scalar) const {
 	Matrix result(r, c);
+
 	for (signed_size_t i = 0; i < r; i++)
 	{
 		for (size_t j = 0; j < c; j++)
@@ -407,42 +400,45 @@ ROBUSTREGRESSION_API inline Matrix Matrix::operator/(const double&scalar) const 
 }
 
 
-ROBUSTREGRESSION_API inline double& Matrix::operator()(const size_t i, const  size_t j)
+ROBUSTREGRESSION_API  double& Matrix::operator()(const size_t i, const  size_t j)
 {
 	return  m[i * c + j];
 }
-ROBUSTREGRESSION_API inline const double& Matrix::operator()(const size_t i, const  size_t j)const
+
+
+ROBUSTREGRESSION_API  const double& Matrix::operator()(const size_t i, const  size_t j)const
 {
 	return m[i * c + j];
 }
 
 
 
-ROBUSTREGRESSION_API inline size_t Matrix::Rows() const
+ROBUSTREGRESSION_API  size_t Matrix::Rows() const
 {
 	return this->r;
 }
 
-ROBUSTREGRESSION_API inline size_t Matrix::Columns() const
+ROBUSTREGRESSION_API  size_t Matrix::Columns() const
 {
 	return this->c;
 }
 
-ROBUSTREGRESSION_API inline Matrix Matrixcode::Transpose(const Matrix &m)
+ROBUSTREGRESSION_API  Matrix Matrixcode::Transpose(const Matrix &m)
 {
 	size_t c = m.Columns();
 	size_t r = m.Rows();
 	Matrix t(c, r);
 	for (signed_size_t i = 0; i < c; i++)
 	{
-		for (size_t j = 0; j < r; j++) {
+		for (size_t j = 0; j < r; j++)
+		{
 			t(i, j) = m(j, i);
 		}
 	}
 	return t;
 }
 
-ROBUSTREGRESSION_API inline void Matrixcode::printmatrix(const Matrix &m)
+ROBUSTREGRESSION_API  void Matrixcode::printmatrix(const Matrix &m)
 {
 	for (signed_size_t i = 0; i < m.Rows(); i++)
 	{
