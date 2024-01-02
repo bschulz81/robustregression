d92a18d929a55a7b7d69f42c378247e40df262d6
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2024-01-02 05:39:36 +0100

    Update setup.py
    version update

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
 name="pyRobustRegressionLib",
-version="1.3.1",
+version="1.3.2",
 description="A library that implements algorithms for linear and non-linear robust regression.",
 long_description=long_description,
 long_description_content_type='text/markdown',
